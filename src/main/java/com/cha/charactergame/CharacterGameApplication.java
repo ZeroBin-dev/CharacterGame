b5a1866e100253600f5d5964751671584e41fdfb
CYB <zero-bin@naver.com>
2024-09-30 09:56:09 +0900

    init

---
+++ src/main/java/com/cha/charactergame/CharacterGameApplication.java
@@ -0,0 +1,13 @@
+package com.cha.charactergame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharacterGameApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CharacterGameApplication.class, args);
+  }
+
+}
